0d98b37009ef92446c9ae54cc4d8b9d0ce378ad5
ocarrolm <morganocarroll01@gmail.com>
2025-12-05 23:06:41 +0000

    Window open with SDL

--- viewer.c
+++ viewer.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h> // Graphics Library, will need to include
 
 int main() {
-    print("Hello World!\n");
+    printf("Hello World!\n");
+
+    SDL_Window *pwindow = SDL_CreateWindow("Image Viewer",
+        SDL_WINDOWPOS_CENTERED, // x
+        SDL_WINDOWPOS_CENTERED, // y
+        900, // height
+        600, // width
+        0 // Flags
+    );
+    
+    SDL_Delay(3000);
 }
