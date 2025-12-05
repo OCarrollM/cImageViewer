05d109a3990521b4ab52f2b268dd823e8442c522
ocarrolm <morganocarroll01@gmail.com>
2025-12-05 23:21:00 +0000

    Pixel

--- viewer.c
+++ viewer.c
@@ -13,5 +13,18 @@ int main() {
         0 // Flags
     );
     
+    SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
+
+    Uint8 r,g,b;
+    r = 0xFF;
+    g = b = 0x00;
+    Uint32 color = SDL_MapRGB(psurface->format,r,g,b);
+
+    int x = 50;
+    int y = 50;
+    const SDL_Rect pixel = (SDL_Rect){x,y,1,1};
+    SDL_FillRect(psurface, &pixel, color);
+    
+    SDL_UpdateWindowSurface(pwindow);
     SDL_Delay(3000);
 }
